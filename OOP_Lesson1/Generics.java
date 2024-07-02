0d48f33bcd368a7074a7e54c89cdd35b48a89f5a
Михаил Лубенцов <wispergently061@gmail.com>
2024-07-02 19:30:30 +0500

    Первая инициализация проекта.

---
+++ OOP_Lesson1/Generics.java
@@ -0,0 +1,12 @@
+package OOP_Lesson1;
+import static print_module.print_library.println;
+
+/**
+ * Generics
+ */
+public class Generics {
+
+    public static void main(String[] args) {
+        println("Hello");
+    }
+}
